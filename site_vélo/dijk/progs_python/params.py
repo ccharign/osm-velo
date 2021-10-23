7c03d951adafb12ee6b3eb1bdf6cb2c02a1f1d57
cyril charignon <cyril.charignon@net-c.com>
2021-10-23 12:49:59 +0200

    changement des adresses de fichiers

--- site_vélo/dijk/progs_python/params.py
+++ site_vélo/dijk/progs_python/params.py
@@ -6,22 +6,29 @@ import sys
 sys.path.append("dijk/progs_python")
 sys.path.append("dijk/")
 
-RACINE_PROJET="dijk/progs_python/"
 
 
-CHEMIN_XML = os.path.join(RACINE_PROJET, "données/voies_et_nœuds.osm")  #Adresse du fichier .osm élagué utilisé pour chercher les nœuds d'une rue.
+### Adresses des fichiers de données ###
 
-CHEMIN_XML_COMPLET = os.path.join(RACINE_PROJET,"données_inutiles/pau_agglo.osm")
-CHEMIN_RUE_NUM_COORDS = os.path.join(RACINE_PROJET,"données/rue_num_coords.csv")
-CHEMIN_NŒUDS_VILLES = os.path.join(RACINE_PROJET,"données/nœuds_villes.csv")
-CHEMIN_NŒUDS_RUES = os.path.join(RACINE_PROJET,"données/nœuds_rues.csv")
-CHEMIN_CACHE = os.path.join(RACINE_PROJET,"données/cache_adresses.csv")
-CHEMIN_CYCLA = os.path.join(RACINE_PROJET,"données/Cyclabilité.csv")
-CHEMIN_CHEMINS = os.path.join(RACINE_PROJET,"données/chemins.csv")
-DONNÉES = os.path.join(RACINE_PROJET,"données")
+RACINE_PROJET="dijk/"
 
+DONNÉES = os.path.join(RACINE_PROJET,"données/Pau")
 TMP = os.path.join(RACINE_PROJET, "tmp/")
 os.makedirs(TMP, exist_ok=True)
+os.makedirs(DONNÉES, exist_ok=True)
+
+CHEMIN_XML = os.path.join(DONNÉES, "voies_et_nœuds.osm")  #Adresse du fichier .osm élagué utilisé pour chercher les nœuds d'une rue.
+CHEMIN_XML_COMPLET = os.path.join(TMP,"pau_agglo.osm") # le .osm complet. Mis dans TMP pour ne pas être transféré sur github
+CHEMIN_RUE_NUM_COORDS = os.path.join(DONNÉES,"rue_num_coords.csv")
+CHEMIN_NŒUDS_VILLES = os.path.join(DONNÉES,"nœuds_villes.csv")
+CHEMIN_NŒUDS_RUES = os.path.join(DONNÉES,"nœuds_rues.csv")
+CHEMIN_CACHE = os.path.join(DONNÉES,"cache_adresses.csv")
+CHEMIN_CYCLA = os.path.join(DONNÉES,"Cyclabilité.csv")
+CHEMIN_CHEMINS = os.path.join(DONNÉES,"chemins.csv")
+
+
+
+### Réglages divers ###
 
 STR_VILLE_DÉFAUT = "64000 Pau"  #Lorsque la ville n'est pas précisée par l'utilisateur
 PAYS_DÉFAUT = "France"
@@ -30,10 +37,11 @@ PAYS_DÉFAUT = "France"
 D_MAX_POUR_NŒUD_LE_PLUS_PROCHE = 500 #en mètres
 
 
+### logs ###
 os.makedirs(os.path.join(RACINE_PROJET, "log"), exist_ok=True)
 def LOG_PB(msg):
     f = open(os.path.join(RACINE_PROJET,"log/pb.log"), "a")
     f.write(f"{datetime.now()}   {msg}\n")
     f.close()
     print(msg)
-LOG_PB("nouveau chargement de params.py\n\n")
+LOG_PB("Nouveau chargement de params.py\n\n")
