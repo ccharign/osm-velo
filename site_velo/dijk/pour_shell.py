041a8d899abaa657a3e0f983427917a39c74f15d
cyril charignon <cyril.charignon@net-c.com>
2022-04-02 09:51:16 +0200

    détails

--- site_velo/dijk/pour_shell.py
+++ site_velo/dijk/pour_shell.py
@@ -2,7 +2,8 @@
 from importlib import reload
 import dijk.models as mo
 import dijk.views as v
-from progs_python.initialisation.initialisation import charge_ville, charge_zone, À_RAJOUTER_PAU
 import progs_python.initialisation.vers_django as vd
+import progs_python.initialisation.initialisation as ini
+import ini.charge_fichier_cycla_défaut as charge_cycla_defaut
 from progs_python.utils import lecture_tous_les_chemins, réinit_cycla
 from django.db import close_old_connections
