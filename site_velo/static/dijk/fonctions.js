cf4bbaec10a33aac43d6d9065f5817d380a7e501
cyril charignon <cyril.charignon@net-c.com>
2022-06-12 18:37:45 +0200

    plus de variable globale laCarte

--- site_velo/static/dijk/fonctions.js
+++ site_velo/static/dijk/fonctions.js
@@ -2,6 +2,25 @@
 var nbÉtapes = 0;
 var nbArêtesInterdites = 0;
 
+
+// https://stackoverflow.com/questions/2360655/jquery-event-handlers-always-execute-in-order-they-were-bound-any-way-around-t
+// [name] is the name of the event "click", "mouseover", .. 
+// same as you'd pass it to bind()
+// [fn] is the handler function
+$.fn.bindFirst = function(name, fn) {
+    // bind as you normally would
+    // don't want to miss out on any jQuery magic
+    this.bind(name, fn);
+
+    // Thanks to a comment by @Martin, adding support for
+    // namespaced events too.
+    var handlers = this.data('events')[name.split('.')[0]];
+    // take out the handler we just inserted from the end
+    var handler = handlers.pop();
+    // move it at the beginning
+    handlers.splice(0, 0, handler);
+};
+
 function gèreLesClics(carte){
     carte.on("click",
 	     e => addMarker(e, carte)
